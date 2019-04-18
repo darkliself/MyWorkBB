8fd9fd5b7886f86c7969fe6e560b90f2bf2ba1d5
Darkliself <4e486bux@gmail.com>
2019-04-18 16:57:42 +0300

    18.04

--- temp ref.cs
+++ temp ref.cs
@@ -13,3 +13,6 @@ R("SP-18656").HasValue() ? R("SP-18656").Replace("<NULL>", "").Text :
 
         @"(?<=d) (?=(yd(s)|mm|cm|m|L|mL|gram|cup(s)|qt|MB/s)( |$||))",
         @"(?<=\d) (?=(yd\(s\)|mm|cm|m|L|mL|gram|cup\(s\)|qt|MB\/s)( |$|\|))",
+
+
+Add(Coalesce(test).RegexReplace("([A-Z]{2,})|([A-Z][a-z])", "$1".ToLower()));
