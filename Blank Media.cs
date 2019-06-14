17d77716e60a6f4ea124795c5e41f75108f0b23f
Darkliself <4e486bux@gmail.com>
2019-06-14 17:27:37 +0300

    up

--- Blank Media.cs
+++ Blank Media.cs
@@ -1,31 +1,31 @@
-//§§ "Blank Media" "Alex K."
+//§§548610392096142807 "Blank Media" "Alex K."
 
 // --[FEATURE #1]
-// --
+// --Blank media type & Use
 
 // --[FEATURE #2]
-// --
+// --Max speed supported, True color & Printable
 
 // --[FEATURE #3]
-// --
+// --Blank media data capacity
 
 // --[FEATURE #4]
-// --
+// --Maximum video recording time (minutes)/Maximum audio recording time (minutes)
 
 // --[FEATURE #5]
-// --
+// --Compatible with
 
 // --[FEATURE #6]
-// --
+// --Blank media package type
 
 // --[FEATURE #7]
-// --
+// --Blank media grade or quality
 
 // --[FEATURE #8]
-// --
+// --Rewritable
 
 // --[FEATURE #9]
-// --
+// --Media pack size (If more than 1)
 
 // --[FEATURE #10]
 // --
@@ -34,6 +34,6 @@
 // --
 
 // --[FEATURE #12]
-// --
+// --Warranty information
 
-//§§ end of "Blank Media"
+//§§548610392096142807 end of "Blank Media"
